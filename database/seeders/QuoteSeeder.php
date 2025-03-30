6fdc318217ef8c9a13d2f41f15c6aa547d273212
Cesar Octavio Delgado <codelgado@Cesars-MacBook-Pro.local>
2025-03-30 11:03:04 -0400

    Fix Quote Factory

--- database/seeders/QuoteSeeder.php
+++ database/seeders/QuoteSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Quote;
 
 class QuoteSeeder extends Seeder
 {
